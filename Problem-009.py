73a3dd2cbd5ad3eb893b50d62fccd36770367678
Belve D Marks <onodric@yahoo.com>
2018-03-11 22:57:33 -0500

    Refactoring into classes and methods

--- Problem-009.py
+++ Problem-009.py
@@ -1,42 +1,50 @@
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+class Problem_009:
+    """
+    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-# a2 + b2 = c2
-# For example, 32 + 42 = 9 + 16 = 25 = 52.
+    a2 + b2 = c2
 
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product abc.
+    For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
-def possibles(nnnn): # where nnnn is the target sum
-	a = 1
-	b = 1
-	c = 1
-	trips = []
-	for i in range(1,nnnn):
-		for j in range(1,nnnn):
-			for k in range(1,nnnn):
-				if i + j + k == nnnn:
-					one = [i, j, k]
-					trips.append(one)
-					print trips
-	return trips
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
+    """
 
-# print len(possibles(1000))
-# print "end"
+    def get_pythagorean_trips_for(self, total):
+        """
+        Method to find Pythagorean triplets that sum to a given integer
+        Arguments: <integer> total
+        Returns: Array<Array<integer>> Pythagrean Triplet
+        """
+        trips = []
+        for i in range(1, total-2):
+            for j in range(1, total-2):
+                for k in range(1, total-2):
+                    if i + j + k == total:
+                        one = [i, j, k]
+                        trips.append(one)
+        return trips
 
-def triple(mmmm): # where mmmm is the target sum
-	trips = []
-	tester = []
-	winner = []
-	for a in range(1, mmmm):
-		for b in range(1, mmmm):
-			for c in range(1, mmmm):
-				if a**2 + b **2 == c**2:
-					winner = [a, b, c]
-					trips.append(winner)
-					print winner
-	for i in range(len(trips)):
-		tester = trips[i]
-		if tester[0] + tester[1] + tester[2] == 1000:
-			return tester[0] * tester[1] * tester[2]
+    def get_product_of_pyth_trips_summing_to(self, total):
+        """
+        Method to calculate the product of three numbers that are pytagorean triplets
+        Arguments: <integer> total
+        Return: <integer> product
+        """
+        trips = self.get_pythagorean_trips_for(total)
+        products = []
+        for i in range(len(trips)):
+            tester = trips[i]
+            if tester[0] + tester[1] + tester[2] == total:
+                products.append(tester[0] * tester[1] * tester[2])
+        return products
 
-print triple(1000)
+
+if __name__ == "__main__":
+    import sys
+    solver = Problem_009()
+    arg = int(sys.argv[1])
+    print("The products of all Pythagorean triples that sum to {} are:".format(arg))
+    solutions = solver.get_product_of_pyth_trips_summing_to(arg)
+    for i in range(len(solutions)):
+        print("\t{}".format(solutions[i]))
