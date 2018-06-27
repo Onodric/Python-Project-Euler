843ddc901dbf385561671b5467f6754fcbde3cbf
Onodric <onodric@yahoo.com>
2018-06-27 06:49:33 -0500

    tweaked testing module file

--- test/test_factorize.py
+++ test/test_factorize.py
@@ -3,26 +3,42 @@ from helpers import factorize
 
 
 class Problem_001_Spec(unittest.TestCase):
-    """The test cases for 002
-
-    See Above
+    """The test cases for helpers.factorize
 
     Methods:
-
+        setup(self, *args, **kwargs):
+        test_get_all_prime_factors_of_happy_path(self):
+        test_get_all_prime_factors_of_null_path(self):
+        test_get_all_prime_factors_of_zero_path(self):
+        test_get_all_factor_pairs_of_happy_path(self):
+        test_get_all_factor_pairs_of_null_path(self):
+        test_get_all_factor_pairs_of_zero_path(self):
     """
 
     def setup(self, *args, **kwargs):
         self.problem = factorize.Factorize()
 
-    def test_happy_path(self):
+    def test_get_all_prime_factors_of_happy_path(self):
         self.setup()
         self.assertTrue(False)
 
-    def test_null_path(self):
+    def test_get_all_prime_factors_of_null_path(self):
         self.setup()
         self.assertTrue(False)
 
-    def test_zero_path(self):
+    def test_get_all_prime_factors_of_zero_path(self):
+        self.setup()
+        self.assertTrue(False)
+
+    def test_get_all_factor_pairs_of_happy_path(self):
+        self.setup()
+        self.assertTrue(False)
+
+    def test_get_all_factor_pairs_of_null_path(self):
+        self.setup()
+        self.assertTrue(False)
+
+    def test_get_all_factor_pairs_of_zero_path(self):
         self.setup()
         self.assertTrue(False)
 
--- testme.py
+++ testme.py
@@ -1,6 +1,11 @@
 import sys
 import subprocess
 
+"""
+to run:
+python testme.py --unittest
+"""
+
 if __name__ == '__main__':
     if '--unittest' in sys.argv:
         subprocess.call([sys.executable, '-m', 'unittest', 'discover'])
